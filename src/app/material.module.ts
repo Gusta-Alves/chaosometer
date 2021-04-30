bcbe6005aaf13bf0354dd71bb87829087aa8b200
gusta_alves <gustavoalves1010@gmail.com>
2021-04-30 20:10:47 -0300

    distribuição de tabela e adição de icones

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,12 +6,14 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import {MatTableModule} from '@angular/material/table';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 const materiais = [
   MatExpansionModule,
   MatTableModule,
   MatCardModule,
   MatToolbarModule,
+  MatIconModule,
 ]
 
 @NgModule({
